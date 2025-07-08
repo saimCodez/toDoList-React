7d49cc60cd5a61fcb40327f624e47069c90cf514
SAIMXKHAN <saim.tauseef27@gmail.com>
2025-07-08 20:01:47 +0500

    change

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import { ToDoList } from "./components/toDoList";
-import { Counter } from "./components/counter";
+
 const App = () => {
   return (
     <div>
       <ToDoList />
-      {/* <Counter/> */}
+      
     </div>
   );
 };
